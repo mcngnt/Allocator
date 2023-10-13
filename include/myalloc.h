24b30a452b72125596aa65aacfbd1544c2997b69
mcngnt <martin.cuingnet@icloud.com>
2023-10-13 13:55:20 +0200

    P3

--- include/myalloc.h
+++ include/myalloc.h
@@ -1,21 +1,7 @@
 #include <stdio.h>
-
-
-#define MAX_SMALL 100
-
-
-#define SIZE_BLK_SMALL 128 - sizeof(size_t)
-
-// Struct used to represent a block of memory
-struct BlockSmall
-{
-	// The LSB of the header tells if the block is in use (LSB = 1) or not (LSB = 0)
-	size_t header;
-	// Body of the block
-	char body[SIZE_BLK_SMALL];
-};
-
-
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
 
 
 // Memory test functions //
@@ -26,6 +12,7 @@ int is_memory_safe(void* ptr);
 
 // Memory management functions //
 
+// Initialize memory headers by setting up the chained list of free blocks
 void initialize_memory();
 // Returns a pointer to the body of a memory block
 void* myMalloc(size_t size);
@@ -37,22 +24,23 @@ void* myRealloc(void* ptr, size_t size);
 
 // Memory reading and writing functions //
 
-// Returns int at a pointer adress in memory after checking that the pointer is valid
+// Returns int at a pointer address in memory after checking that the pointer is valid
 int read_safe_int(void* ptr);
-// Returns char at a pointer adress in memory after checking that the pointer is valid
+// Returns char at a pointer address in memory after checking that the pointer is valid
 char read_safe_char(void* ptr);
-// Writes int at a pointer adress in memory after checking that the pointer is valid
+// Writes int at a pointer address in memory after checking that the pointer is valid
 void write_safe_int(void* ptr,int value);
-// Writes char at a pointer adress in memory after checking that the pointer is valid
+// Writes char at a pointer address in memory after checking that the pointer is valid
 void write_safe_char(void* ptr,char value);
 
 
 // Debug functions //
 
 // Shows the content of a block by displaying the asci representation of each of its bytes
-void print_block_content(unsigned int blockID);
+void print_block_content(void* ptr);
 // Shows which blocks of memory are used ( o for free and x if used)
 void print_blocks_used();
+
 
 
 // Test functions //
@@ -63,6 +51,7 @@ void test_free2();
 void test_malloc1();
 void test_malloc2();
 void test_realloc();
-void header_test();
+void test_header();
+void speed_test(size_t testNB);
 
 
